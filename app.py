ae200c54c419d775a7f6ac4d0fed82fa8f9699c2
Shreyas Rao <109731624+Humbledot00@users.noreply.github.com>
2025-03-01 14:52:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,8 +19,7 @@ DB_CONNECTION_URL = os.getenv("DB_CONNECTION_URL")
 # MongoDB connection
 mongo_client = MongoClient(DB_CONNECTION_URL)
 # Set up OpenAI API key
-client = OpenAI()
-client.api_key = API_KEY
+client = OpenAI(api_key=os.getenv("API_KEY"))
 
 
 db = mongo_client["learning_db"]  # Database name
